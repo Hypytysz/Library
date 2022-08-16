55f7a57dd8d6fd207d0a4912ac1113dee74181ba
Hypytysz <arek1nowosielski@gmail.com>
2022-08-16 10:34:43 +0200

    Dodanie szczegolow postu, edycji i usuwania po zalogowaniu

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,7 @@ from posts import views
 urlpatterns = [
     path("", views.posts, name="posts"),
     path('__debug__/', include('debug_toolbar.urls')),
-    path("details/<int:pk>", views.post_details, name="postdetails")
+    path("details/<int:pk>", views.post_details, name="postdetails"),
+    path("edit/<int:pk>", views.post_edit, name="edit"),
+    path("delete/<int:pk>", views.delete_post, name="delete"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from posts.models import Post
@@ -21,7 +21,23 @@ def post_details(request, pk):
     if request.method == "POST":
         title = request.POST.get("title")
         content = request.POST.get("content")
+        author = request.POST.get("author")
+        post.title = title
+        post.content = content
+        post.author = author
+    return render(request, 'posts/details.html', {"post":post})
+
+def post_edit(request, pk):
+    post= Post.objects.get(pk=pk)
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
         post.title = title
         post.content = content
         post.save()
-    return render(request, 'posts/details.html', {"post":post})
+        return redirect("posts")
+    return render(request, 'posts/edit.html', {"post":post})
+
+def delete_post(request, pk):
+    Post.objects.filter(pk=pk).delete()
+    return redirect("posts")
